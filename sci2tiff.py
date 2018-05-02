6bdc6e13608b9f93376900eb1e3aeef3a4383185
Kevin M. Gill <kmsmgill@gmail.com>
2018-05-02 13:42:06 -0700

    Added new stretching options (min/max values, std multiple); Added output format option; Bug fixes. Used this version to process this: https://www.flickr.com/photos/kevinmgill/41137228044/in/dateposted/

--- sci2tiff.py
+++ sci2tiff.py
@@ -1,3 +1,4 @@
+
 import sys
 import os
 import numpy as np
@@ -8,9 +9,12 @@ from astropy.io import fits
 import vicar
 import re
 
-
 def load_fits_matrix(input_file, band=0):
     hdu_list = fits.open(input_file)
+
+    if band + 1 > len(hdu_list):
+        raise Exception("Band '%d' not found in data"%band)
+
     pixel_matrix = hdu_list[band].data
 
     pixel_matrix = np.flipud(pixel_matrix)
@@ -19,9 +23,6 @@ def load_fits_matrix(input_file, band=0):
 
 def load_vic_matrix(input_file):
     pixel_matrix, value_pairs = vicar.load_vic(input_file)
-
-
-
     return pixel_matrix
 
 def load_image_matrix(input_file, band=0):
@@ -89,8 +90,8 @@ def get_data_min_max(input_file,
                                 maxpercent,
                                 None,
                                 trim=trim,
-                                convert_to_16bit=False,
-                                stretch=False)
+                                stretch=False,
+                                outputformat=None)
 
     pixel_min = np.nanmin(pixel_matrix)
     pixel_max = np.nanmax(pixel_matrix)
@@ -121,15 +122,16 @@ def process_data(pixel_matrix,
             dohisteq=False,
             minpercent=None,
             maxpercent=None,
+            std_mult=None,
             resize=None,
             trim=0,
-            convert_to_16bit=True,
-            stretch=True):
+            stretch=True,
+            outputformat="uint16"):
 
     if type(trim) == int and trim > 0:
         pixel_matrix = pixel_matrix[trim:-trim, trim:-trim]
 
-    # Scale to 0-65535 and convert to UInt16
+
     if force_input_min is not None:
         pixel_min = force_input_min
     else:
@@ -139,6 +141,7 @@ def process_data(pixel_matrix,
         pixel_max = force_input_max
     else:
         pixel_max = np.nanmax(pixel_matrix)
+
 
     if fill_null_stripes is True:
         stripes = detect_null_stripes(pixel_matrix)
@@ -159,27 +162,44 @@ def process_data(pixel_matrix,
         inds = np.where(np.isnan(pixel_matrix))
         pixel_matrix[inds] = np.nanmax(pixel_matrix)
 
+
+    if std_mult is not None:
+        std = pixel_matrix.std()
+        pixel_min = pixel_matrix.mean() - std * std_mult
+        pixel_max = pixel_matrix.mean() + std * std_mult
+
     if stretch is True:
         pixel_matrix = pixel_matrix - pixel_min
         pixel_matrix = pixel_matrix / (pixel_max - pixel_min)
         pixel_matrix[pixel_matrix < 0] = 0
 
-    # Format for UInt16
-    if convert_to_16bit is True:
-        pixel_matrix = pixel_matrix * 65535.0
-        pixel_matrix = pixel_matrix.astype(np.uint16)
-
-    if dohisteq is True:
-        pixel_matrix = histeq(pixel_matrix)
-
     if resize is not None:
-        if re.match("\A\d+x\d+\Z", "1024x1024") is None:
+        if re.match("\A\d+x\d+\Z", resize) is not None:
+            new_size = map(int, re.split("x", resize))
+        elif re.match("\A\d+[\.]*[\d]*%\Z", resize) is not None:
+            new_size = (np.array((pixel_matrix.shape[1], pixel_matrix.shape[0])) * (float(resize[:resize.index("%")]) / 100.0)).astype(np.int32)
+        else:
             print "Invalid resize specifier:", resize
             sys.exit(0)
 
-        new_size = map(int, re.split("x", resize))
+        pixel_matrix = imresize(pixel_matrix, size=new_size, interp='bicubic', mode='F')
 
-        pixel_matrix = imresize(pixel_matrix, size=new_size, interp='bicubic')
+    if outputformat == "byte" or outputformat == "uint8":
+        pixel_matrix = pixel_matrix * 256.0
+        pixel_matrix = pixel_matrix.astype(np.uint8)
+    elif outputformat == "uint16":
+        pixel_matrix = pixel_matrix * 65535.0
+        pixel_matrix = pixel_matrix.astype(np.uint16)
+    elif outputformat == "uint32":
+        pixel_matrix = pixel_matrix * 4294967295.0
+        pixel_matrix = pixel_matrix.astype(np.uint32)
+    elif outputformat == "float16":
+        pixel_matrix = pixel_matrix.astype(np.float16)
+    elif outputformat == "float32":
+        pixel_matrix = pixel_matrix.astype(np.float32)
+
+    if dohisteq is True:
+        pixel_matrix = histeq(pixel_matrix, nbr_bins=65535)
 
     return pixel_matrix
 
@@ -192,9 +212,11 @@ def sci2tiff(input_file,
             dohisteq=False,
             minpercent=None,
             maxpercent=None,
+            std_mult=None,
             resize=None,
             band=0,
-            trim=0):
+            trim=0,
+            outputformat="uint16"):
 
     pixel_matrix = load_image_matrix(input_file, band=band)
     pixel_matrix = process_data(pixel_matrix,
@@ -205,8 +227,10 @@ def sci2tiff(input_file,
                                 dohisteq,
                                 minpercent,
                                 maxpercent,
+                                std_mult,
                                 resize,
-                                trim)
+                                trim,
+                                outputformat=outputformat)
 
     output_filename = build_output_filename(input_file)
     print "Writing", output_filename
@@ -230,6 +254,10 @@ if __name__ == "__main__":
     parser.add_argument("-r", "--resize", help="Resize image to WidthxHeight", type=str, default=None)
     parser.add_argument("-b", "--band", help="Data band", type=int, default=1)
     parser.add_argument("-t", "--trim", help="Trim borders", type=int, default=0)
+    parser.add_argument("-o", "--outputformat", help="Output format", type=str, default="uint16")
+    parser.add_argument("-X", "--maxvalue", help="Clamp values to maximum value", type=float, default=None)
+    parser.add_argument("-N", "--minvalue", help="Clamp values to minimum value", type=float, default=None)
+    parser.add_argument("-S", "--std", help="Clamp values to within multiple of standard deviations of mean", type=float, default=None)
     args = parser.parse_args()
 
     input_files = args.data
@@ -242,6 +270,11 @@ if __name__ == "__main__":
     resize = args.resize
     band = args.band
     trim = args.trim
+    outputformat = args.outputformat
+    std_mult = args.std
+
+    maxvalue = args.maxvalue
+    minvalue = args.minvalue
 
     input_min = None
     input_max = None
@@ -253,8 +286,11 @@ if __name__ == "__main__":
             data_min, data_max = get_data_min_max(input_file, band=band, trim=trim)
             data_limits += [data_min, data_max]
 
-        input_min = np.array(data_limits).min()
-        input_max = np.array(data_limits).max()
+        input_min = float(np.array(data_limits).min())
+        input_max = float(np.array(data_limits).max())
+    else:
+        input_min = minvalue
+        input_max = maxvalue
 
     for input_file in input_files:
         sci2tiff(input_file,
@@ -265,9 +301,11 @@ if __name__ == "__main__":
                 dohisteq=dohisteq,
                 minpercent=minpercent,
                 maxpercent=maxpercent,
+                std_mult=std_mult,
                 resize=resize,
                 band=band,
-                trim=trim
+                trim=trim,
+                outputformat=outputformat
                 )
 
 
